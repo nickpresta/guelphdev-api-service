1b399c65f08e694bf580db4894cc96a6380d6cad
Nick Presta <nick@nickpresta.ca>
2012-04-11 21:49:29 -0400

    Added an API and documentation.
    * Added API for course fetching.
    * Adding Sphinx documentation support.

---
+++ guelphapi/api/api.py
@@ -0,0 +1,82 @@
+from tastypie import fields
+from tastypie.bundle import Bundle
+from tastypie.exceptions import ImmediateHttpResponse
+from tastypie.http import HttpNotFound
+from tastypie.resources import Resource
+from tastypie.throttle import CacheThrottle
+
+from pyuoguelph import courseparser
+
+class CourseObject(object):
+    def __init__(self, initial=None):
+        self.__dict__['_data'] = {}
+
+        if hasattr(initial, 'items'):
+            self.__dict__['_data'] = initial
+
+    def __getattr__(self, name):
+        return self._data.get(name, None)
+
+    def __setattr__(self, name, value):
+        self.__dict__['_data'][name] = value
+
+    def to_dict(self):
+        return self._data
+
+class CourseResource(Resource):
+    course_code = fields.CharField(attribute='course_code',
+            null=True, readonly=True, unique=True)
+    course_number = fields.CharField(attribute='course_number',
+            null=True, readonly=True, unique=True)
+    course_department = fields.CharField(attribute='course_department',
+            null=True, readonly=True, unique=True)
+    course_title = fields.CharField(attribute='course_title',
+            null=True, readonly=True, unique=True)
+    course_semesters = fields.CharField(attribute='course_semesters',
+            null=True, readonly=True, unique=True)
+    course_credit = fields.CharField(attribute='course_credit',
+            null=True, readonly=True, unique=True)
+    course_description = fields.CharField(attribute='course_description',
+            null=True, readonly=True, unique=True)
+    course_restrictions = fields.CharField(attribute='course_restrictions',
+            null=True, readonly=True, unique=True)
+    course_prereqs = fields.CharField(attribute='course_prereqs',
+            null=True, readonly=True, unique=True)
+    course_uri = fields.CharField(attribute='course_uri',
+            null=True, readonly=True, unique=True)
+
+    class Meta:
+        resource_name = 'course'
+        object_class = CourseObject
+        allowed_methods = ['get']
+        list_allowed_methods = []
+        #throttle = BaseThrottle(throttle_at=10)
+
+    def _client(self):
+        return courseparser.CourseParser()
+
+    def get_resource_uri(self, bundle_or_obj):
+        kwargs = {
+            'resource_name': self._meta.resource_name,
+        }
+
+        if isinstance(bundle_or_obj, Bundle):
+            kwargs['pk'] = bundle_or_obj.obj.course_code
+        else:
+            kwargs['pk'] = bundle_or_obj.couse_code
+
+        if self._meta.api_name is not None:
+            kwargs['api_name'] = self._meta.api_name
+
+        return self._build_reverse_url("api_dispatch_detail", kwargs=kwargs)
+
+    def obj_get(self, request=None, **kwargs):
+        year = request.GET.get('year', '2011')
+        calendar = request.GET.get('calendar', 'undergraduate')
+
+        try:
+            course = self._client().get_course(year, kwargs['pk'], calendar)
+        except courseparser.CourseNotFoundException, e:
+            raise ImmediateHttpResponse(HttpNotFound(e.message))
+
+        return CourseObject(initial=course)
--- guelphapi/urls.py
+++ guelphapi/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import patterns, include, url
 
+from tastypie.api import Api
+from api.api import CourseResource
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+v1_api = Api(api_name='v1')
+v1_api.register(CourseResource())
+
 urlpatterns = patterns('',
+    url(r'^api/', include(v1_api.urls)),
     # Examples:
     # url(r'^$', 'guelphapi.views.home', name='home'),
     # url(r'^guelphapi/', include('guelphapi.foo.urls')),
